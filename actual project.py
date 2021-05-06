f5c0d88c2a210bbc193724ab2db61a7c469a76c5
chirudeepnamini <36069804+chirudeepnamini@users.noreply.github.com>
2021-05-06 13:25:59 +0530

    Add files via upload

---
+++ actual project.py
@@ -0,0 +1,36 @@
+import openpyxl,secrets
+import streamlit as st
+tolerance=st.slider("select a rating",5,9)
+if tolerance==5:
+	file_name='g5.xlsx'
+if tolerance==6:
+	file_name='g6.xlsx'
+if tolerance==7:
+	file_name='g7.xlsx'
+if tolerance==8:
+	file_name='g8.xlsx'
+if tolerance==9:
+	file_name='g9.xlsx'
+wb=openpyxl.load_workbook(file_name)
+sheet=wb['Sheet']
+col=sheet['A']
+tconst=col[secrets.randbelow(sheet.max_row)].value
+from bs4 import BeautifulSoup
+import requests
+url="https://www.imdb.com/title/"+tconst
+print(url)
+r=requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+title=soup.find('h1',attrs={'class':""})
+name=title.text.split('\xa0')[0]
+year=title.text.split('\xa0')[1]
+plot=soup.find('div',attrs={'class':"summary_text"})
+actual_plot=' '.join(plot.text.split())
+print(name)
+print(year)
+print(actual_plot)
+st.write(url)
+st.write('getting movie information...')
+st.write(name)
+st.write(year[1:-1])
+st.write(actual_plot)
