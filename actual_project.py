db98bfc052d8ad64ef8c948f5efc8aa7bbe907f7
chirudeepnamini <36069804+chirudeepnamini@users.noreply.github.com>
2021-05-28 14:29:41 +0530

    Add files via upload

--- actual_project.py
+++ actual_project.py
@@ -1,26 +1,34 @@
 import openpyxl,secrets
 import streamlit as st
+def suggestion(tolerance):
+	if tolerance==5:
+		file_name='g5.xlsx'
+	if tolerance==6:
+		file_name='g6.xlsx'
+	if tolerance==7:
+		file_name='g7.xlsx'
+	if tolerance==8:
+		file_name='g8.xlsx'
+	if tolerance==9:
+		file_name='g9.xlsx'
+	wb=openpyxl.load_workbook(file_name)
+	sheet=wb['Sheet1']
+	col=sheet['A']
+	tconst=col[secrets.randbelow(sheet.max_row)].value
+	return tconst
 tolerance=st.slider("select a rating",5,9)
-if tolerance==5:
-	file_name='g5.xlsx'
-if tolerance==6:
-	file_name='g6.xlsx'
-if tolerance==7:
-	file_name='g7.xlsx'
-if tolerance==8:
-	file_name='g8.xlsx'
-if tolerance==9:
-	file_name='g9.xlsx'
-wb=openpyxl.load_workbook(file_name)
-sheet=wb['Sheet1']
-col=sheet['A']
-tconst=col[secrets.randbelow(sheet.max_row)].value
+tconst=suggestion(tolerance)
 key="9e579f61"
 from bs4 import BeautifulSoup
 import requests,json
-url="http://www.omdbapi.com/?i={}&apikey={}".format(tconst,key)
-r=requests.get(url)
-actcont=json.loads(r.content)
+while(1):
+	url="http://www.omdbapi.com/?i={}&apikey={}".format(tconst,key)
+	r=requests.get(url)
+	actcont=json.loads(r.content)
+	if "Episode" in actcont:
+		tconst=suggestion(tolerance)
+	else:
+		break
 actual_plot=actcont["Plot"]
 year=actcont["Year"]
 name=actcont["Title"]
